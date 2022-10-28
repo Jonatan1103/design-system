b380d0cce51a064ae9ef0f796618ec5a82bf8ade
jonatan timoteo dos santos <jonatan_ts@hotmail.com>
2022-10-28 12:31:57 -0300

    first commit

---
+++ packages/docs/vite.config.js
@@ -0,0 +1,5 @@
+/* eslint-disable prettier/prettier */
+/// <reference types="vite/client" />
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+export default defineConfig({ plugins: [react()] })
